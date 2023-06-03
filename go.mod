1556e381c78dc10da2bed4ce028810b582c8c660
Kon Toshihito <toshihito.kon@gmail.com>
2023-06-03 17:52:21 +0900

    ポジティブスイーツ

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ToshihitoKon/codename-generator
 
 go 1.20
+
+require (
+	github.com/go-faker/faker/v4 v4.1.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
---
+++ random-name.go
@@ -0,0 +1,33 @@
+package main
+
+var sweets = []string{
+	"Cookie", "Chocolate", "Candy", "Marshmallow", "Gummy bear", "Jelly bean",
+	"Cupcake", "Donut", "Brownie", "Ice cream", "Popsicle", "Lollipop", "Caramel",
+	"Toffee", "Macaron", "Peanut brittle", "Fudge", "Cotton candy", "Rock candy",
+	"Jawbreaker", "Muffin", "Pretzel", "Pie", "Churro", "Taffy", "Nougat", "Popcorn",
+	"Tiramisu", "Cheesecake", "Pancake", "Waffle", "Banana bread", "Gingerbread",
+	"Scone", "Shortbread", "Fruitcake", "Custard", "Milkshake", "Pudding",
+	"Gelato", "Sorbet", "Sherbet", "Praline", "Truffle", "Caramel apple",
+	"Caramel corn", "Sugar cookie", "Peanut butter cup", "Candy cane", "Jelly roll",
+	"Baklava", "Halva", "Oreo", "Twix", "Kit Kat", "Snickers", "Skittles", "M&M's",
+	"Reese's", "Hershey's", "Milky Way", "Starburst", "Nerds", "Smarties",
+	"Gobstopper", "Sour Patch Kids", "Pixy Stix", "Airheads", "Twizzlers",
+	"Sour gummy worms", "Jolly Rancher", "Ring Pop", "Peppermint", "Butterscotch",
+	"Licorice", "Mints", "Circus peanuts", "Jawbreakers", "Caramel chew",
+	"Caramel apple", "Tootsie Roll", "Cracker Jack", "Nutter Butter", "S'mores",
+	"Candy corn", "Peeps", "Laffy Taffy", "Butterfinger", "Almond Joy", "Rolo",
+	"Swedish Fish", "Pocky", "Bubble gum", "Chewing gum", "Fruit roll-up",
+	"Fruit by the Foot",
+}
+
+var adjectives = []string{
+	"Awesome", "Brilliant", "Fantastic", "Gorgeous", "Happy", "Incredible", "Joyful",
+	"Kind", "Lovely", "Magnificent", "Nice", "Optimistic", "Perfect", "Radiant",
+	"Spectacular", "Terrific", "Vibrant", "Wonderful", "Adventurous", "Benevolent",
+	"Courageous", "Dazzling", "Elegant", "Friendly", "Gentle", "Harmonious",
+	"Inspiring", "Jubilant", "Lively", "Miraculous", "Nurturing", "Passionate",
+	"Resilient", "Sincere", "Thrilling", "Vivacious", "Youthful", "Blissful",
+	"Charming", "Delightful", "Exquisite", "Grateful", "Heavenly", "Intelligent",
+	"Joyous", "Luminous", "Marvelous", "Peaceful", "Refreshing", "Sunny",
+	"Tranquil", "Victorious",
+}
